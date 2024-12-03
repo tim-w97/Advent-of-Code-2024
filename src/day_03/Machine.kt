3f51a23ae156479d60abd9112f2b3b948b4fa104
Tim Wagner <tim.wagner@check24.de>
2024-12-03 20:46:24 +0100

    solved part 2 of day 3! :-)

--- src/day_03/Machine.kt
+++ src/day_03/Machine.kt
@@ -2,7 +2,7 @@ package day_03
 
 import java.io.File
 
-fun main() {
+fun part1() {
     val instructions = File("src/day_03/input.txt").readText()
 
     val mulRegex = Regex("mul\\([0-9]{1,3},[0-9]{1,3}\\)")
@@ -17,4 +17,32 @@ fun main() {
         .reduce { a, b -> a + b}
 
     println(result)
+}
+
+fun part2() {
+    val instructions = File("src/day_03/input.txt").readText()
+
+    val operationsRegex = Regex("mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don't\\(\\)")
+    val numsRegex = Regex("[0-9]{1,3}")
+    var enabled = true
+    var sum = 0
+
+    operationsRegex.findAll(instructions).forEach { operation ->
+        when {
+            operation.value == "do()" ->
+                enabled = true
+            operation.value == "don't()" ->
+                enabled = false
+            enabled ->
+                sum += numsRegex.findAll(operation.value)
+                    .map { number -> number.value.toInt() }
+                    .reduce { a, b -> a * b}
+        }
+    }
+
+    println(sum)
+}
+
+fun main() {
+    part2()
 }
